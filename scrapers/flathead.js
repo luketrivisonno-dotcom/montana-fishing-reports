24d8fa266a50e8cbc9e317ddfda55118b5b3d960
luketrivisonno-dotcom <luketrivisonno@gmail.com>
2026-02-23 19:47:46 -0700

    Update flathead.js

--- scrapers/flathead.js
+++ scrapers/flathead.js
@@ -1,4 +1,3 @@
-cat > scrapers/flathead.js << 'EOF'
 async function scrapeFlatheadMontanaAngler() {
     return {
         source: 'Montana Angler',
@@ -23,4 +22,3 @@ module.exports = {
     scrapeFlatheadMontanaAngler,
     scrapeFlatheadCrown
 };
-EOF
