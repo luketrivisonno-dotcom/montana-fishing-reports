0ea7f567d1499601f664a8cd5d479073e5a50383
luketrivisonno-dotcom <luketrivisonno@gmail.com>
2026-02-23 22:55:54 -0700

    Create boulder.js

---
+++ scrapers/boulder.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function scrapeBoulder() {
+  // Boulder River reports are covered by Sunrise, Stonefly, George Anderson
+  return [];
+}
+
+module.exports = scrapeBoulder;
