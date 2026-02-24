60df3aaf84c6760a6b4f7a136eec02ade4802842
luketrivisonno-dotcom <luketrivisonno@gmail.com>
2026-02-23 22:55:34 -0700

    Create springcreeks.js

---
+++ scrapers/springcreeks.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function scrapeSpringCreeks() {
+  // Spring Creeks reports are covered by other shops
+  // This is a placeholder for any Spring Creeks specific sources
+  return [];
+}
+
+module.exports = scrapeSpringCreeks;
