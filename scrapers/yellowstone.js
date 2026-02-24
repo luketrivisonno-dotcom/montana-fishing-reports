9f4e5bf677cd0203890cbc30a508b2c0817bb00d
luketrivisonno-dotcom <luketrivisonno@gmail.com>
2026-02-23 19:43:18 -0700

    Update yellowstone.js

--- scrapers/yellowstone.js
+++ scrapers/yellowstone.js
@@ -1,4 +1,3 @@
-cat > scrapers/yellowstone.js << 'EOF'
 async function scrapeYellowstoneMontanaAngler() {
     return {
         source: 'Montana Angler',
@@ -34,4 +33,3 @@ module.exports = {
     scrapeYellowstoneRiversEdge,
     scrapeYellowstoneYellowDog
 };
-EOF
