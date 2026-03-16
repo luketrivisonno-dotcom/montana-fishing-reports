09caf2a119e506ed6cc253e0ccadf9c9753aa6ef
Luke Trivisonno <luketrivisonno@Lukes-MacBook-Pro.local>
2026-03-15 20:45:25 -0600

    Force redeploy

--- server.js
+++ server.js
@@ -2215,3 +2215,4 @@ app.listen(PORT, () => {
 module.exports = { normalizeSource, formatDateForDisplay };
 // Deploy timestamp: Fri Mar 13 17:44:31 MDT 2026
 // Cache clear trigger: Sun Mar 15 19:19:46 MDT 2026
+// Deploy: Sun Mar 15 20:45:25 MDT 2026
